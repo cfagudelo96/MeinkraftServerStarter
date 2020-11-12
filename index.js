b8fc653f1a2c2b292f36a1d801ee3687beb22796
Carlos Agudelo <carlos.agudelo@rappi.com>
2020-11-11 21:52:24 -0500

    General fixes

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-const Discord = require('./node_modules/discord.js')
+const Discord = require('discord.js')
 const puppeteerScript = require('./puppeteer-script')
 
 const client = new Discord.Client();
 
-const prefix = "!";
+const prefix = '!';
 
-client.on("message", function(message) {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
-
-    if (command === "start") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`initializing minecraft server!`);
-        puppeteerScript.launchServer();
-    }
+process.on('uncaughtException', function (err) {
+  console.log("Uncaught exception!");
+  console.error(err);
 });
 
-client.login("Nzc1NTI1NTYwNTc5MjYwNDM5.X6nmdg.uRmboXb-WdhJUQtHZzgCLnrtd0A");
+client.on('message', async function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'start') {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply('Initializing minecraft server!');
+    try {
+      await puppeteerScript.launchServer();
+      message.reply('Ok I clicked the button to start the server! Now we wait...');
+    } catch(error) {
+      if (error === puppeteerScript.SERVER_ON_EXCEPTION) {
+        message.reply('The server is already on you dumb dumb...');
+      } else {
+        message.reply(`I fucked up and couldn't start the server, please ask mama poof for help...`);
+      }
+    }
+  } else if (command === 'status') {
+    try {
+      status = await puppeteerScript.getStatus();
+      if (status === puppeteerScript.STATUS_ON) {
+        message.reply('The server is up, go have some fun!');
+      } else if (status === puppeteerScript.STATUS_OFF) {
+        message.reply('The server is now offline, try turning it on using me~');
+      } else if (status === puppeteerScript.STATUS_LOADING) {
+        message.reply(`The server is loading, don't rush me or I'll bap you`);
+      } else if (status === puppeteerScript.UNKNOWN_STATUS) {
+        message.reply('Ok I have no idea what is the server status right now, call poof for help!');
+      }
+    } catch(error) {
+      message.reply(`I fucked up and I didn't check the server status, please ask mama poof for help...`);
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
--- puppeteer-script.js
+++ puppeteer-script.js
@@ -2,24 +2,50 @@ const puppeteer = require('puppeteer');
 
 const puppeteerScript = {};
 
+puppeteerScript.SERVER_ON_EXCEPTION = new Error("ServerAlreadyOn")
+
+puppeteerScript.STATUS_ON = 'ON';
+
+puppeteerScript.STATUS_OFF = 'OFF';
+
+puppeteerScript.STATUS_LOADING = 'LOADING';
+
+puppeteerScript.UNKNOWN_STATUS = 'UNKNOWN';
+
 puppeteerScript.launchServer = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
+    args: ['--no-sandbox']
   });
   const page = await browser.newPage();
   await page.goto('https://aternos.org/go/');
 
   await signIn(page);
   await selectServer(page);
-  await pushButton(page);
+  await validateServerOff(page);
+  await pushOnButton(page);
 
   await browser.close();
 }
 
+puppeteerScript.getStatus = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  });
+  const page = await browser.newPage();
+  await page.goto('https://aternos.org/go/');
+
+  await signIn(page);
+  await selectServer(page);
+  const status = await getServerStatus(page);
+  await browser.close();
+  return status;
+}
+
 async function signIn(page) {
-  // TODO: Use environment variables or something
-  const username = 'MeinkraftServerBot';
-  const password = 'MeinkraftServerBot1!'
+  const username = process.env.ATERNOS_USERNAME;
+  const password = process.env.ATERNOS_PASSWORD;
 
   await page.type('#user', username);
   await page.type('#password', password);
@@ -30,17 +56,39 @@ async function signIn(page) {
 }
 
 async function selectServer(page) {
-  // TODO: Use environment variables or something
-  //serverId = 'yVkfHQgKvEMW6mVi';
-  serverButton = await page.$('.server');
+  serverId = process.env.ATERNOS_SERVER_ID;
+  serverButton = await page.$(`[data-id=${serverId}]`);
   await serverButton.click();
 
   await page.waitForNavigation();
 }
 
-async function pushButton(page) {
+async function pushOnButton(page) {
   startButton = await page.$('#start');
   await startButton.click();
+}
+
+async function validateServerOff(page) {
+  serverOnElement = await page.$('.server-actions.online');
+  if (serverOnElement !== null) {
+    throw puppeteerScript.SERVER_ON_EXCEPTION;
+  }
+}
+
+async function getServerStatus(page) {
+  onElement = await page.$('.server-status .status.online');
+  if (onElement !== null) {
+    return puppeteerScript.STATUS_ON;
+  }
+  offElement = await page.$('.server-status .status.offline');
+  if (offElement !== null) {
+    return puppeteerScript.STATUS_OFF;
+  }
+  loadingElement = await page.$('.server-status .status.loading');
+  if (loadingElement !== null) {
+    return puppeteerScript.STATUS_LOADING;
+  }
+  return puppeteerScript.UNKNOWN_STATUS;
 }
 
 module.exports = puppeteerScript;
